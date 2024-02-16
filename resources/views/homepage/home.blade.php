5f9837eabe35696e8e495c354e8508c91e8f0475
sadique-cws <sadique.hussain96@gmail.com>
2024-02-16 08:50:05 +0530

    fetch error resolve

--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -63,13 +63,13 @@
                         let serviceList = $("#serviceList")
                         serviceList.empty();
 
-                        let services = response.services;
+                        let services = response;
 
                         services.forEach( (item) => {
                             serviceList.append(`
                                 <div class="flex-1 ">
-                                    <a href="" class="flex flex-col items-center gap-2     hover:bg-slate-100 bg-slate-50 px-2 py-4 rounded-lg">
-                                        <img  class="max-w-16" src="product_image/${item.icon}" alt="">
+                                    <a href="" class="flex flex-col items-center gap-2 hover:bg-slate-100 bg-slate-50 px-2 py-4 rounded-lg">
+                                        <img  class="max-w-16" src="uploads/${item.icon}" alt="">
                                         <h3 class="text-xs">${item.name}</h3>
                                     </a>
                                 </div>
