cd123cec2cfee01c08e2b1252a6f749208014381
roni836 <ronitsaha836@gmail.com>
2024-02-18 21:03:15 +0530

    book_appointment_ajax

--- app/Http/Controllers/AppointmentApiController.php
+++ app/Http/Controllers/AppointmentApiController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Appointment;
 use Illuminate\Http\Request;
 
 class AppointmentApiController extends Controller
@@ -11,7 +11,7 @@ class AppointmentApiController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(["appointments"=>Appointment::all()]);
     }
 
     /**
@@ -27,8 +27,33 @@ class AppointmentApiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'fullname' => 'required|string|max:255',
+            'mobileno' => 'required|string|max:12',
+            'address' => 'required|string',
+            'landmark' => 'required|string',
+            'city' => 'required|string',
+            'requirement_id'=>'required',
+            'preferred_date' => 'required|date',
+            'preferred_time' => 'required|time',
+        ]);
+    
+
+        // Store the service
+        $appointment = new appointment();
+        $appointment->fullname = $request->fullname;
+        $appointment->mobileno = $request->mobileno;
+        $appointment->address = $request->address;
+        $appointment->landmark = $request->landmark;
+        $appointment->city = $request->city;
+        $appointment->requirement_id = $request->requirement_id;
+        $appointment->preferred_date = $request->preferred_date;
+        $appointment->preferred_time = $request->preferred_time;
+        $appointment->save();
+      
+        return response()->json(['success' => true,'data'=>$appointment,'msg'=>'Appointment has been successfully Booked']);
     }
+
 
     /**
      * Display the specified resource.
