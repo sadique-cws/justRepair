f6fd045fab7e0fa49bfae70cce837157e6035e1e
sadique-cws <sadique.hussain96@gmail.com>
2024-02-22 17:57:16 +0530

    commit admin merger

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,6 @@ use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\ServiceFeesController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::match(['get','post'],'/login',[AuthController::class,"login"])->name("login");
 Route::post('/register',[AuthController::class,"registerNew"])->name("register");
 Route::post('/logout',[AuthController::class,"logout"])->name("logout");
