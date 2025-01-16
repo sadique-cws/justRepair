2c8bc669f8aeee17fa80bb9d53454a0157f61958
khobabe <khobabe007@gmail.com>
2025-01-16 15:21:28 +0530

    new appintment counting and showing updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
     public function dashboard(){
         $data['count_appointment'] = Appointment::all()->count();
         $data['count_user'] = User::all()->count();
-        $data['count_accepted_appointments'] = Appointment::where('status','accept')->count();
+        $data['count_accepted_appointments'] = Appointment::whereIn('status',['accept','process'])->count();
         return view("admin.dashboard", $data);
     }
 
