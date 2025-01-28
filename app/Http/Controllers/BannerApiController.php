b54c404a2a31bff892ccccf4d79c12270e352b8c
khobabe <khobabe007@gmail.com>
2025-01-28 19:08:45 +0530

    banner call done in homepage . carousel is incomplete

--- app/Http/Controllers/BannerApiController.php
+++ app/Http/Controllers/BannerApiController.php
@@ -22,7 +22,7 @@ class BannerApiController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg,avif|max:2048',
         ]);
 
         $banner = new Banner(); 
