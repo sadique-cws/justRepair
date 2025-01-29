96be13e034913dcba467ff98cd2bd89684c61284
khobabe <khobabe007@gmail.com>
2025-01-29 16:28:37 +0530

    automatic sliding updated using vanilla script

--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -60,49 +60,6 @@
                 </div>
             </div>
 
-            {{-- Slide work goes here --}}
-            {{-- <div id="default-carousel" class="relative flex-1" data-carousel="slide">
-                <div id="bannerImage" class="relative h-[600px] overflow-hidden rounded-lg">
-                    <img src="https://this-person-does-not-exist.com/en" alt="">
-                </div>
-                <button type="button"
-                    class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                    data-carousel-prev>
-                    <span
-                        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30  group-hover:bg-white/50  group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-                        <svg class="w-4 h-4 text-white  rtl:rotate-180" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 1 1 5l4 4" />
-                        </svg>
-                        <span class="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button"
-                    class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                    data-carousel-next>
-                    <span
-                        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30  group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-                        <svg class="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span class="sr-only">Next</span>
-                    </span>
-                </button>
-
-            </div> --}}
-
-
-            {{-- <div id="default-carousel" class="relative flex-1" data-carousel="slide">
-                <!-- Carousel wrapper -->
-                <div id="bannerImage" class="carousel-wrapper relative h-56 overflow-hidden rounded-lg md:h-96">
-
-                </div>
-
-            </div> --}}
-
             <div class="relative flex-1 h-56 md:h-96 overflow-hidden rounded-lg">
                 <!-- Carousel Wrapper -->
                 <div id="carousel" class="flex transition-transform duration-500 ease-in-out rounded-lg bg-red-500"></div>
@@ -135,7 +92,6 @@
                     data: {
                         'search': search
                     },
-                    // processData:false,
                     success: function(response) {
                         $('#loader').hide();
 
@@ -168,40 +124,11 @@
                 }
             });
 
-            //banner calling goes here:
-            // let callingBanner = () => {
-            //     $.ajax({
-            //         type: "GET",
-            //         url: "/api/admin/banner",
-            //         success: function(response) {
-            //             // console.log(response);
-            //             let banners = $('#bannerImage');
-            //             banners.empty();
-            //             let data = response;
-            //             // console.log(data);
-
-            //             data.forEach((banner, index) => {
-            //                 banners.append(`                                
-        //                     <div class="${index === 0 ?? 'active'} duration-700 ease-in-out" data-carousel-item>
-        //                         <img src="/banners/${banner.image}" alt="banner-${index + 1}" 
-        //                         class="absolute block w-full h-full object-cover">
-        //                     </div>                                
-        //                 `);
-            //             });
-            //         },
-            //         error: function(err) {
-            //             console.error("Error loading banners:", err);
-            //         }
-            //     });
-            // }
-            // callingBanner();
-
 
             let images = [];
             let currentIndex = 0;
-            let autoSlideInterval; //store interval refrence
+            let autoSlideInterval; 
 
-            // Fetch images using jQuery AJAX
             function loadImages() {
                 $.ajax({
                     type: "GET",
@@ -209,7 +136,6 @@
                     dataType: 'json',
                     success: function(response) {
                         console.log(response);
-                        // images = response; // Assuming data is an array of image URLs
                         images = response.map(item => `/banners/${item.image}`);
                         displayImages();
                         startAutoSlide();
@@ -244,23 +170,21 @@
             });
 
             $('#next').click(() => {
-                resetAutoSlide(); //reset timer when manually sliding
+                resetAutoSlide();
                 currentIndex = (currentIndex + 1) % images.length;
                 updateCarousel();
             });
 
-            // Automatic sliding every 3 seconds:
             function startAutoSlide() {
                 autoSlideInterval = setInterval(() => {
                     currentIndex = (currentIndex + 1) % images.length;
                     updateCarousel();
-                }, 3000); // Change slide every 3 seconds
+                }, 3000); 
             }
 
-            // Reset auto-slide when user interacts manually
             function resetAutoSlide() {
-                clearInterval(autoSlideInterval); // Stop the current interval
-                startAutoSlide(); // Restart the auto-slide
+                clearInterval(autoSlideInterval); 
+                startAutoSlide(); 
             }
 
             loadImages();
