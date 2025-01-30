a3cc30fccc38192deeaaca7fdb3ea487f260ff42
khobabe <khobabe007@gmail.com>
2025-01-30 14:58:52 +0530

    update gap in whatsapp icon

--- resources/views/homepage/layout.blade.php
+++ resources/views/homepage/layout.blade.php
@@ -203,19 +203,9 @@
                 <span class="text-sm text-gray-500  group-hover:text-blue-600  text-nowrap">My
                     Services</span>
             </a>
-            {{-- <a href="{{ route('profile') }}"
-                class="inline-flex flex-col items-center justify-center px-3 hover:bg-gray-50  group">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6 text-slate-500">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                </svg>
-
-                <span
-                    class="text-sm text-gray-500  group-hover:text-blue-600 ">Profile</span>
-            </a> --}}
+            
             <a href="https://wa.me/+917280080080" target="_blank"
-                class="inline-flex items-center gap-2 text-white px-3 py-2 rounded-md flex-col">
+                class="inline-flex items-center text-white px-3 py-2 rounded-md flex-col">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"
                     class="w-6 h-6 text-gray-500 rounded-full">
                     <path
